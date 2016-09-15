edc930ba1fa4e52903b397a631c52c0e0d289231
whucyb <zgcyb@163.com>
2016-09-15 19:30:51 +0800

    Update 2.py

--- Exercise_02/2.py
+++ Exercise_02/2.py
@@ -30,8 +30,8 @@ CHAR=raw_input()                     #输入字符串
 char=list(CHAR)                      #将字符串转换为list
 length=len(char)                     #计算list长度
 for i in range(length):
-    char[i]=ord(char[i])-65         #将list中每一个字符对应的ASCII码转换为其在alphabet中的索引位置
-for j in range(7):                  #将所输入的字符串中字母对应图案分行输出
+    char[i]=ord(char[i])-65          #将list中每一个字符对应的ASCII码转换为其在alphabet中的索引位置
+for j in range(7):                   #将所输入的字符串中字母对应图案分行输出
     for k in char:
-        print alphabet[k][j],' ',   #输出所输入的字符串中所有字母对应图案的其中一行
-    print                           #输出完一行后换行
+        print alphabet[k][j],' ',    #输出所输入的字符串中所有字母对应图案的其中一行
+    print                            #输出完一行后换行
