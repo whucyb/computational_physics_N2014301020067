fc738b9d9bd2c5cdbf1e122f3663eae0cb32d959
whucyb <zgcyb@163.com>
2016-10-17 01:45:22 +0800

    Update Exercise_05.py

--- Exercise_05/Exercise_05.py
+++ Exercise_05/Exercise_05.py
@@ -18,10 +18,10 @@ class trajectory(object):
             self.v_x[-1].append([self.v_0 * math.cos((float(i) / 180) * math.pi)])
             self.v_y[-1].append([self.v_0 * math.sin((float(i) / 180) * math.pi)])
             while (self.y[-1][-1][-1] > 0) or (self.x[-1][-1][-1] == 0):
-                self.v_x[-1][-1].append(self.v_x[-1][-1][-1])
-                self.v_y[-1][-1].append(self.v_y[-1][-1][-1] - g * self.delta_t)
                 self.x[-1][-1].append(self.x[-1][-1][-1] + self.v_x[-1][-1][-1] * self.delta_t)
                 self.y[-1][-1].append(self.y[-1][-1][-1] + self.v_y[-1][-1][-1] * self.delta_t)
+                self.v_x[-1][-1].append(self.v_x[-1][-1][-1])
+                self.v_y[-1][-1].append(self.v_y[-1][-1][-1] - g * self.delta_t)
             if self.y[-1][-1][-1] < 0:
                 r = - (self.y[-1][-1][-2] / self.y[-1][-1][-1])
                 self.x[-1][-1][-1] = (self.x[-1][-1][-2] + r * self.x[-1][-1][-1]) / (r + 1)    
@@ -91,10 +91,10 @@ class g_depand_on_altitude(trajectory):
             self.v_x[-1].append([self.v_0 * math.cos((float(i) / 180) * math.pi)])
             self.v_y[-1].append([self.v_0 * math.sin((float(i) / 180) * math.pi)])
             while (self.y[-1][-1][-1] > 0) or (self.x[-1][-1][-1] == 0):
-                self.v_x[-1][-1].append(self.v_x[-1][-1][-1])
-                self.v_y[-1][-1].append(self.v_y[-1][-1][-1] - g_0 * ((float(R) / (R + self.y[-1][-1][-1])) ** 2) * self.delta_t)
                 self.x[-1][-1].append(self.x[-1][-1][-1] + self.v_x[-1][-1][-1] * self.delta_t)
                 self.y[-1][-1].append(self.y[-1][-1][-1] + self.v_y[-1][-1][-1] * self.delta_t)
+                self.v_x[-1][-1].append(self.v_x[-1][-1][-1])
+                self.v_y[-1][-1].append(self.v_y[-1][-1][-1] - g_0 * ((float(R) / (R + self.y[-1][-1][-1])) ** 2) * self.delta_t)
             if self.y[-1][-1][-1] < 0:
                 r = - (self.y[-1][-1][-2] / self.y[-1][-1][-1])
                 self.x[-1][-1][-1] = (self.x[-1][-1][-2] + r * self.x[-1][-1][-1]) / (r + 1)
@@ -187,12 +187,87 @@ class with_density_correction(with_air_drag):
         plt.ylim(0,)
         plt.text(1200,800,'With air drag\nWith density correction\nIsothermal model')
         plt.subplot(133)
-        for j in range(len(self.initial_angle)):
-            plt.plot(self.x[3][j], self.y[3][j])
+        for k in range(len(self.initial_angle)):
+            plt.plot(self.x[3][k], self.y[3][k])
         plt.title('Trajectory of cannon shell')
         plt.xlabel('x (km)')
         plt.ylabel('y (km)')
         plt.xlim(0,)
         plt.ylim(0,)
         plt.text(5,70,'With air drag\nWith density correction\nAdiabatic model')
+        plt.show()
+class with_density_and_g_correction(with_density_correction):
+    def calculate_isothermal_model(self):
+        self.x.append([])
+        self.y.append([])
+        self.v_x.append([])
+        self.v_y.append([])
+        g_0 = 0.0098
+        R = 6371
+        B_2_divided_by_m = 0.04
+        y_0 = 10
+        for i in self.initial_angle:
+            self.x[-1].append([0])
+            self.y[-1].append([0])
+            self.v_x[-1].append([self.v_0 * math.cos((float(i) / 180) * math.pi)])
+            self.v_y[-1].append([self.v_0 * math.sin((float(i) / 180) * math.pi)])
+            while (self.y[-1][-1][-1] > 0) or (self.x[-1][-1][-1] == 0):
+                v = math.sqrt((self.v_x[-1][-1][-1]) ** 2 + (self.v_y[1][-1][-1]) ** 2)
+                self.x[-1][-1].append(self.x[-1][-1][-1] + self.v_x[-1][-1][-1] * self.delta_t)
+                self.y[-1][-1].append(self.y[-1][-1][-1] + self.v_y[-1][-1][-1] * self.delta_t)
+                self.v_x[-1][-1].append(self.v_x[-1][-1][-1] - math.exp( - self.y[-1][-1][-1] / y_0) * B_2_divided_by_m * v * self.v_x[-1][-1][-1] * self.delta_t)
+                self.v_y[-1][-1].append(self.v_y[-1][-1][-1] - (g_0 * ((float(R) / (R + self.y[-1][-1][-1])) ** 2) + math.exp( - self.y[-1][-1][-1] / y_0) * B_2_divided_by_m * v * self.v_y[-1][-1][-1]) * self.delta_t)
+            if self.y[-1][-1][-1] < 0:
+                r = - (self.y[-1][-1][-2] / self.y[-1][-1][-1])
+                self.x[-1][-1][-1] = (self.x[-1][-1][-2] + r * self.x[-1][-1][-1]) / (r + 1)
+    def calculate_adiabatic_model(self):
+        self.x.append([])
+        self.y.append([])
+        self.v_x.append([])
+        self.v_y.append([])
+        g_0 = 0.0098
+        R = 6371
+        B_2_divided_by_m = 0.04
+        a = 0.0065
+        alpha = 2.5
+        T_0 = 300
+        for i in self.initial_angle:
+            self.x[-1].append([0])
+            self.y[-1].append([0])
+            self.v_x[-1].append([self.v_0 * math.cos((float(i) / 180) * math.pi)])
+            self.v_y[-1].append([self.v_0 * math.sin((float(i) / 180) * math.pi)])
+            while (self.y[-1][-1][-1] > 0) or (self.x[-1][-1][-1] == 0):
+                v = math.sqrt((self.v_x[-1][-1][-1]) ** 2 + (self.v_y[1][-1][-1]) ** 2)
+                self.x[-1][-1].append(self.x[-1][-1][-1] + self.v_x[-1][-1][-1] * self.delta_t)
+                self.y[-1][-1].append(self.y[-1][-1][-1] + self.v_y[-1][-1][-1] * self.delta_t)
+                self.v_x[-1][-1].append(self.v_x[-1][-1][-1] - ((1 - a * self.y[-1][-1][-1] / T_0) ** alpha) * B_2_divided_by_m * v * self.v_x[-1][-1][-1] * self.delta_t)
+                self.v_y[-1][-1].append(self.v_y[-1][-1][-1] - (g_0 * ((float(R) / (R + self.y[-1][-1][-1])) ** 2) + ((1 - a * self.y[-1][-1][-1] / T_0) ** alpha) * B_2_divided_by_m * v * self.v_y[-1][-1][-1]) * self.delta_t)
+            if self.y[-1][-1][-1] < 0:
+                r = - (self.y[-1][-1][-2] / self.y[-1][-1][-1])
+                self.x[-1][-1][-1] = (self.x[-1][-1][-2] + r * self.x[-1][-1][-1]) / (r + 1)
+    def show_results(self):
+        plt.subplot(131)
+        for i in range(len(self.initial_angle)):
+            plt.plot(self.x[0][i], self.y[0][i])
+        plt.title('Trajectory of cannon shell\nNo grag\ng is a constant')
+        plt.xlabel('x (km)')
+        plt.ylabel('y (km)')
+        plt.xlim(0,60)
+        plt.ylim(0,20)
+        plt.subplot(132)
+        for j in range(len(self.initial_angle)):
+            plt.plot(self.x[1][j], self.y[1][j])
+        plt.title('Trajectory of cannon shell\nWith air drag  isothermal_model\ng depends on altitude')
+        plt.xlabel('x (km)')
+        plt.ylabel('y (km)')
+        plt.xlim(0,60)
+        plt.ylim(0,20)
+        plt.subplot(133)
+        for k in range(len(self.initial_angle)):
+            plt.plot(self.x[2][k], self.y[2][k])
+        plt.title('Trajectory of cannon shell\nWith air drag  adiabatic_model\ng depends on altitude')
+        plt.xlabel('x (km)')
+        plt.ylabel('y (km)')
+        plt.xlim(0,60)
+        plt.ylim(0,20)
         plt.show()
